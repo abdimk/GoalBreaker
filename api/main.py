339d56d0829db0f71c06ed5b8a81b1051f7ae11a
abdimk <abdisamerga@yahoo.com>
2025-11-25 13:38:56 -0800

    updated

--- api/main.py
+++ api/main.py
@@ -1,8 +1,21 @@
-from fastapi import FastAPI
+import sys
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from models.database import engine, Base
 from routes.router import router
+
+project_root = Path(__file__).resolve().parent
+sys.path.append(str(project_root))
+
+origins = [
+    "http://localhost:3000",
+    "https://goal-breaker-one.vercel.app"
+]
+
+allow_origin_regex = r"https://.*\.vercel\.app"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,19 +25,30 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+app.include_router(router)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://goal-breaker-one.vercel.app"],
+    allow_origins=origins,
+    allow_origin_regex=allow_origin_regex,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-app.include_router(router)
-
 @app.get("/")
 async def index():
     return {"Server is working"}
+
+@app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+async def catch_all_routes(request: Request, full_path: str):
+    return JSONResponse(
+        status_code=404,
+        content={
+            "error": "Not Found",
+            "message": f"The path '/{full_path}' does not exist. Try /api/..."
+        }
+    )
 
 if __name__ == "__main__":
     import uvicorn
