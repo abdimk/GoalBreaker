88f2cb91d6532e5fc3f8365d97600b5570773756
abdimk <abdisamerga@yahoo.com>
2025-11-25 13:28:43 -0800

    updated

--- api/main.py
+++ api/main.py
@@ -16,7 +16,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://goal-breaker-one.vercel.app/"],
+    allow_origins=["https://goal-breaker-one.vercel.app","https://goal-breaker-one.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
